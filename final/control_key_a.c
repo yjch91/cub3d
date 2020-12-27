7174dafc871b4ebe12d25c17b72e9b2f6844d5fc
Jaechul Yun <jayun@c3r3s5.42seoul.kr>
2020-12-28 02:25:29 +0900

    lrlr

--- final/control_key_a.c
+++ final/control_key_a.c
@@ -6,23 +6,23 @@
 /*   By: jayun <jayun@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 01:42:13 by jayun             #+#    #+#             */
-/*   Updated: 2020/12/28 01:57:45 by jayun            ###   ########.fr       */
+/*   Updated: 2020/12/28 02:24:53 by jayun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	control_key_a_x(t_info *info, int n)
+static void	control_key_a_x(t_info *info, int n, double vdir_x)
 {
 	if (n == 0)
-		info->pos_x += info->dir_x * info->movespeed;
+		info->pos_x -= vdir_x * info->movespeed;
 	else if (n == 4 || n == 5 || n == 6)
 	{
-		info->map[(int)(info->pos_x + info->dir_x * info->movespeed)]
+		info->map[(int)(info->pos_x - vdir_x * info->movespeed)]
 			[(int)(info->pos_y)] = 0;
-		remove_sprite(info, (int)(info->pos_x + info->dir_x * info->movespeed),
+		remove_sprite(info, (int)(info->pos_x - vdir_x * info->movespeed),
 				(int)(info->pos_y));
-		info->pos_x += info->dir_x * info->movespeed;
+		info->pos_x -= vdir_x * info->movespeed;
 		if (n == 4 && (info->hp + 10) <= 150)
 			info->hp += 10;
 		else if (n == 5)
@@ -32,17 +32,17 @@ static void	control_key_a_x(t_info *info, int n)
 	}
 }
 
-static void	control_key_a_y(t_info *info, int n)
+static void	control_key_a_y(t_info *info, int n, double vdir_y)
 {
 	if (n == 0)
-		info->pos_y += info->dir_y * info->movespeed;
+		info->pos_y -= vdir_y * info->movespeed;
 	else if (n == 4 || n == 5 || n == 6)
 	{
 		info->map[(int)(info->pos_x)]
-			[(int)(info->pos_y + info->dir_y * info->movespeed)] = 0;
+			[(int)(info->pos_y - vdir_y * info->movespeed)] = 0;
 		remove_sprite(info, (int)(info->pos_x),
-				(int)(info->pos_y + info->dir_y * info->movespeed));
-		info->pos_y += info->dir_y * info->movespeed;
+				(int)(info->pos_y - vdir_y * info->movespeed));
+		info->pos_y -= vdir_y * info->movespeed;
 		if (n == 4 && (info->hp + 10) <= 150)
 			info->hp += 10;
 		else if (n == 5)
@@ -54,12 +54,18 @@ static void	control_key_a_y(t_info *info, int n)
 
 void		control_key_a(t_info *info)
 {
-	int	n;
+	int		n;
+	double	len;
+	double	vdir_x;
+	double	vdir_y;
 
-	n = info->map[(int)(info->pos_x + info->dir_x * info->movespeed)]
+	len = sqrt(info->plane_x * info->plane_x + info->plane_y * info->plane_y);
+	vdir_x = info->plane_x / len;
+	vdir_y = info->plane_y / len;
+	n = info->map[(int)(info->pos_x - vdir_x * info->movespeed)]
 		[(int)(info->pos_y)];
-	control_key_a_x(info, n);
+	control_key_a_x(info, n, vdir_x);
 	n = info->map[(int)(info->pos_x)]
-		[(int)(info->pos_y + info->dir_y * info->movespeed)];
-	control_key_a_y(info, n);
+		[(int)(info->pos_y - vdir_y * info->movespeed)];
+	control_key_a_y(info, n, vdir_y);
 }
